dc7d9026e8878e9295a65bbacd29682577b58f36
xiaoxiae <tomas.slama.131@gmail.com>
2019-02-17 17:23:57 +0100

    add code snippet support

--- compile.py
+++ compile.py
@@ -1,7 +1,7 @@
 import os
 
-os.system("lualatex soc.tex")
+os.system("lualatex --shell-escape soc.tex")
 os.system("bibtex soc")
 os.system("makeglossaries soc")
-os.system("lualatex soc.tex")
-os.system("lualatex soc.tex")
+os.system("lualatex --shell-escape soc.tex")
+os.system("lualatex --shell-escape soc.tex")
