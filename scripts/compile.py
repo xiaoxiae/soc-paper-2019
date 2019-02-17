19dca4f2044a4382834d3935b6b69307165c0194
xiaoxiae <tomas.slama.131@gmail.com>
2019-02-17 23:50:15 +0100

    comment compile.py
    Since the other .py script is commented...

--- scripts/compile.py
+++ scripts/compile.py
@@ -2,8 +2,15 @@ import os
 
 os.chdir("..")
 
+# initial lualatex run
 os.system("lualatex --shell-escape soc.tex")
+
+# build bibliography
 os.system("bibtex soc")
+
+# build glossaries
 os.system("makeglossaries soc")
+
+# run lualatex twice to correctly typeset cross-reference-related text
 os.system("lualatex --shell-escape soc.tex")
 os.system("lualatex --shell-escape soc.tex")
