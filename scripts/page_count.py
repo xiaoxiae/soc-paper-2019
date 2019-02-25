0b9aeb91836ba9313cc85d34e40d68b271ad5493
xiaoxiae <tomas.slama.131@gmail.com>
2019-02-25 22:05:59 +0100

    update page_count.py
    The script prints additional information and works on Linux too.

--- scripts/page_count.py
+++ scripts/page_count.py
@@ -1,12 +1,14 @@
 import os
 
 # get the number of characters and words of the text
-characters = os.popen('texcount.pl -0 -sum -utf8 -char ../soc.tex').read()
-words = os.popen('texcount.pl -0 -sum -utf8 ../soc.tex').read()
+characters = os.popen('perl texcount.pl -0 -sum -utf8 -char ../soc.tex').read()
+words = os.popen('perl texcount.pl -0 -sum -utf8 ../soc.tex').read()
 
 # calculate the number of regular pages
 # the number of spaces is approximately the number of words
-page_count = (int(characters) + int(words))  / 1800
+page_count = (int(characters) + int(words)) / 1800
 
 # print the rounded number of regular pages
-input("Počet normostran: " + str(round(page_count, 2))+"\n")
+print("Normostrany: " + str(round(page_count, 2)))
+print("Slova: " + str(words).strip())
+print("Znaky: " + str(characters).strip())
