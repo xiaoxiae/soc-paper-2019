688ace9b324318f79abe052e2d2b5fe1a70a595f
xiaoxiae <tomas.slama.131@gmail.com>
2019-03-13 19:00:49 +0100

    remove hardcoded tex file names from page_count.py

--- scripts/page_count.py
+++ scripts/page_count.py
@@ -1,14 +1,15 @@
 import os
 
-# get the number of characters and words of the text
-characters = os.popen('perl texcount.pl -0 -sum -utf8 -char ../soc.tex').read()
-words = os.popen('perl texcount.pl -0 -sum -utf8 ../soc.tex').read()
+# this makes it so that the name of the tex file isn't hardcoded
+file = [f for f in os.listdir(".") if f.endswith(".tex")][0]
 
-# calculate the number of regular pages
+# get the number of characters and words of the text
+characters = os.popen('perl texcount.pl -0 -sum -utf8 -char ../' + file).read()
+words = os.popen('perl texcount.pl -0 -sum -utf8 ../' + file).read()
+
 # the number of spaces is approximately the number of words
 page_count = (int(characters) + int(words)) / 1800
 
-# print the rounded number of regular pages
 print("Normostrany: " + str(round(page_count, 2)))
 print("Slova: " + str(words).strip())
 print("Znaky: " + str(characters).strip())
