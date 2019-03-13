cded9358f431aef1a803903ea0153f1b80ca8779
xiaoxiae <tomas.slama.131@gmail.com>
2019-03-13 18:57:55 +0100

    remove hardcoded tex file names from compile.py

--- scripts/compile.py
+++ scripts/compile.py
@@ -1,25 +1,27 @@
 import os
+import subprocess
 
-# the path to this file
+# easier to change for running the tex commands
 file_dir = os.path.dirname(os.path.abspath(__file__))
-
-# change working directory to one below this file
 os.chdir(file_dir + os.sep + "..")
 
-# clean-up files from the previous compilation
+# this makes it so that the name of the tex file isn't hardcoded
+file_name = [f for f in os.listdir(".") if f.endswith(".tex")][0][:-len(".tex")]
+
+# clean-up files from the previous compilation to perform a clean one
 for file in [f for f in os.listdir(".") if os.path.isfile(f)]:
-    if file.startswith("soc") and not file.endswith(("tex", "bib", "xoj")):
-        os.remove(file)
+    if file.startswith(file_name) and not file.endswith(("tex", "bib", "xoj")):
+            os.remove(file)
 
 # initial lualatex run
-os.system("lualatex --shell-escape soc.tex")
+os.system("lualatex --shell-escape " + file_name + ".tex")
 
 # build bibliography
-os.system("biber soc")
+os.system("biber " + file_name)
 
 # build glossaries
-os.system("makeglossaries soc")
+os.system("makeglossaries " + file_name)
 
 # run lualatex twice to correctly typeset cross-reference-related text
-os.system("lualatex --shell-escape soc.tex")
-os.system("lualatex --shell-escape soc.tex")
+os.system("lualatex --shell-escape " + file_name + ".tex")
+os.system("lualatex --shell-escape " + file_name + ".tex")
